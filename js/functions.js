1a978e28a7efcb18f53e0ef9669c5e66c8cf9867
Huevos <79326577+CyHuevos@users.noreply.github.com>
2021-03-16 08:47:47 -0600

    Add files via upload

--- js/functions.js
+++ js/functions.js
@@ -14,15 +14,12 @@ btnCerrarPopup.addEventListener('click', function(e) {
     popup.classList.remove('active');
 });
 
-/*function setTxt(){
-                document.sett.settxt.value=30
-}*/
 
 function unCheck(){
-    var op1=document.getElementById("opc1");
-    var op2=document.getElementById("opc2");
-    var op3=document.getElementById("opc3");
-    var op4=document.getElementById("opc4");
+    var op1=document.getElementById("opc1-1");
+    var op2=document.getElementById("opc1-2");
+    var op3=document.getElementById("opc1-3");
+    var op4=document.getElementById("opc1-4");
 
     op1.onclick=function(){
         if(op1.checked != false){ 
@@ -59,10 +56,10 @@ function unCheck(){
 }
 
 function unCheck2(){
-    var op1=document.getElementById("opc11");
-    var op2=document.getElementById("opc21");
-    var op3=document.getElementById("opc31");
-    var op4=document.getElementById("opc41");
+    var op1=document.getElementById("opc3-1");
+    var op2=document.getElementById("opc3-2");
+    var op3=document.getElementById("opc3-3");
+    var op4=document.getElementById("opc3-4");
 
     op1.onclick=function(){
         if(op1.checked != false){ 
@@ -99,10 +96,250 @@ function unCheck2(){
 }
 
 function unCheck3(){
-    var op1=document.getElementById("opc12");
-    var op2=document.getElementById("opc22");
-    var op3=document.getElementById("opc32");
-    var op4=document.getElementById("opc42");
+    var op1=document.getElementById("opc5-1");
+    var op2=document.getElementById("opc5-2");
+    var op3=document.getElementById("opc5-3");
+    var op4=document.getElementById("opc5-4");
+
+    op1.onclick=function(){
+        if(op1.checked != false){ 
+            op2.checked =null; 
+            op3.checked=null;
+            op4.checked=null;
+        }
+    }
+
+    op2.onclick=function(){
+        if(op2.checked != false){ 
+            op1.checked =null; 
+            op3.checked=null;
+            op4.checked=null;
+        }
+    }
+
+
+    op3.onclick=function(){
+        if(op3.checked != false){ 
+            op2.checked =null; 
+            op1.checked=null;
+            op4.checked=null;
+        }
+    }
+
+    op4.onclick=function(){
+        if(op4.checked != false){ 
+            op2.checked =null; 
+            op3.checked=null;
+            op1.checked=null;
+        }
+    }                
+}
+
+function unCheck4(){
+    var op1=document.getElementById("opc2-1");
+    var op2=document.getElementById("opc2-2");
+    var op3=document.getElementById("opc2-3");
+    var op4=document.getElementById("opc2-4");
+
+    op1.onclick=function(){
+        if(op1.checked != false){ 
+            op2.checked =null; 
+            op3.checked=null;
+            op4.checked=null;
+        }
+    }
+
+    op2.onclick=function(){
+        if(op2.checked != false){ 
+            op1.checked =null; 
+            op3.checked=null;
+            op4.checked=null;
+        }
+    }
+
+
+    op3.onclick=function(){
+        if(op3.checked != false){ 
+            op2.checked =null; 
+            op1.checked=null;
+            op4.checked=null;
+        }
+    }
+
+    op4.onclick=function(){
+        if(op4.checked != false){ 
+            op2.checked =null; 
+            op3.checked=null;
+            op1.checked=null;
+        }
+    }
+}              
+
+function unCheck5(){
+    var op1=document.getElementById("opc4-1");
+    var op2=document.getElementById("opc4-2");
+    var op3=document.getElementById("opc4-3");
+    var op4=document.getElementById("opc4-4");
+
+    op1.onclick=function(){
+        if(op1.checked != false){ 
+            op2.checked =null; 
+            op3.checked=null;
+            op4.checked=null;
+        }
+    }
+
+    op2.onclick=function(){
+        if(op2.checked != false){ 
+            op1.checked =null; 
+            op3.checked=null;
+            op4.checked=null;
+        }
+    }
+
+
+    op3.onclick=function(){
+        if(op3.checked != false){ 
+            op2.checked =null; 
+            op1.checked=null;
+            op4.checked=null;
+        }
+    }
+
+    op4.onclick=function(){
+        if(op4.checked != false){ 
+            op2.checked =null; 
+            op3.checked=null;
+            op1.checked=null;
+        }
+    }
+}              
+
+function unCheck6(){
+    var op1=document.getElementById("opc6-1");
+    var op2=document.getElementById("opc6-2");
+    var op3=document.getElementById("opc6-3");
+    var op4=document.getElementById("opc6-4");
+
+    op1.onclick=function(){
+        if(op1.checked != false){ 
+            op2.checked =null; 
+            op3.checked=null;
+            op4.checked=null;
+        }
+    }
+
+    op2.onclick=function(){
+        if(op2.checked != false){ 
+            op1.checked =null; 
+            op3.checked=null;
+            op4.checked=null;
+        }
+    }
+
+
+    op3.onclick=function(){
+        if(op3.checked != false){ 
+            op2.checked =null; 
+            op1.checked=null;
+            op4.checked=null;
+        }
+    }
+
+    op4.onclick=function(){
+        if(op4.checked != false){ 
+            op2.checked =null; 
+            op3.checked=null;
+            op1.checked=null;
+        }
+    }                
+}
+
+function unCheck7(){
+    var op1=document.getElementById("opc7-1");
+    var op2=document.getElementById("opc7-2");
+    var op3=document.getElementById("opc7-3");
+    var op4=document.getElementById("opc7-4");
+
+    op1.onclick=function(){
+        if(op1.checked != false){ 
+            op2.checked =null; 
+            op3.checked=null;
+            op4.checked=null;
+        }
+    }
+
+    op2.onclick=function(){
+        if(op2.checked != false){ 
+            op1.checked =null; 
+            op3.checked=null;
+            op4.checked=null;
+        }
+    }
+
+
+    op3.onclick=function(){
+        if(op3.checked != false){ 
+            op2.checked =null; 
+            op1.checked=null;
+            op4.checked=null;
+        }
+    }
+
+    op4.onclick=function(){
+        if(op4.checked != false){ 
+            op2.checked =null; 
+            op3.checked=null;
+            op1.checked=null;
+        }
+    }                
+}
+
+function unCheck8(){
+    var op1=document.getElementById("opc8-1");
+    var op2=document.getElementById("opc8-2");
+    var op3=document.getElementById("opc8-3");
+    var op4=document.getElementById("opc8-4");
+
+    op1.onclick=function(){
+        if(op1.checked != false){ 
+            op2.checked =null; 
+            op3.checked=null;
+            op4.checked=null;
+        }
+    }
+
+    op2.onclick=function(){
+        if(op2.checked != false){ 
+            op1.checked =null; 
+            op3.checked=null;
+            op4.checked=null;
+        }
+    }
+
+
+    op3.onclick=function(){
+        if(op3.checked != false){ 
+            op2.checked =null; 
+            op1.checked=null;
+            op4.checked=null;
+        }
+    }
+
+    op4.onclick=function(){
+        if(op4.checked != false){ 
+            op2.checked =null; 
+            op3.checked=null;
+            op1.checked=null;
+        }
+    }                
+}
+
+function unCheck9(){
+    var op1=document.getElementById("opc9-1");
+    var op2=document.getElementById("opc9-2");
+    var op3=document.getElementById("opc9-3");
+    var op4=document.getElementById("opc9-4");
 
     op1.onclick=function(){
         if(op1.checked != false){ 
@@ -139,20 +376,14 @@ function unCheck3(){
 }
 
 function res(){
-    var tr1=document.getElementById("opc1");
-    var fl2=document.getElementById("opc2");
-    var fl3=document.getElementById("opc3");
-    var fl4=document.getElementById("opc4");
-
-    var tr2=document.getElementById("opc11");
-    var fl5=document.getElementById("opc21");
-    var fl6=document.getElementById("opc31");
-    var fl7=document.getElementById("opc41");
-
-    var tr3=document.getElementById("opc12");
-    var fl8=document.getElementById("opc22");
-    var fl9=document.getElementById("opc32");
-    var fl10=document.getElementById("opc42");
+    var tr1=document.getElementById('opc1-4');
+    var tr2=document.getElementById('opc2-2');
+    var tr3=document.getElementById('opc3-4');
+    var tr4=document.getElementById('opc4-3');
+    var tr5=document.getElementById('opc5-3');
+    var tr6=document.getElementById('opc6-2');
+    var tr7=document.getElementById('opc7-3');
+    var tr8=document.getElementById('opc8-2');
 
     var re=0;
     var nr=0;
@@ -174,6 +405,36 @@ function res(){
     	re=re+1;
     }else{
     	nr=nr+1;
+    }
+
+    if(tr4.checked==true){
+        re=re+1;
+    }else{
+        nr=nr+1;
+    }
+
+    if (tr5.checked==true) {
+        re=re+1;
+    }else{
+        nr=nr+1;
+    }
+
+    if (tr6.checked==true) {
+        re=re+1;
+    }else{
+        nr=nr+1;
+    }
+
+    if (tr7.checked==true) {
+        re=re+1;
+    }else{
+        nr=nr+1;
+    }
+
+    if (tr8.checked==true) {
+        re=re+1;
+    }else{
+        nr=nr+1;
     }
 
     var sre=''+re+'/'+10+'';
